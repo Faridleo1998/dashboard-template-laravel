5f5975ffdf5fdac3cef3ce81c54379d021a4962c
Farid Moreno <farid.leonardo98@gmail.com>
2023-09-19 10:46:08 -0500

    feat(Components): import input to search records and add search functionality

--- resources/js/Components/Common/Table.vue
+++ resources/js/Components/Common/Table.vue
@@ -1,10 +1,12 @@
 <script setup>
+import { watch } from 'vue'
 import ButtonAction from '../Buttons/ButtonAction.vue'
+import Input from '../Forms/Input.vue'
 import NoDataMessage from './NoDataMessage.vue'
 import Status from './Status.vue'
 import SvgIcon from './SvgIcon.vue'
-import { computed } from 'vue'
 import { router } from '@inertiajs/vue3'
+import { computed, ref } from 'vue'
 
 const props = defineProps({
   columns: {
@@ -30,18 +32,46 @@ const props = defineProps({
   checks: {
     type: Boolean,
     default: false
+  },
+  search: {
+    type: Boolean,
+    default: true
+  },
+  placeholderSearch: {
+    type: String,
+    default: 'Buscar...'
+  },
+  filters: {
+    type: Object,
+    default: () => {}
   }
 })
+
+const searchText = ref(props.filters.search)
 
 const gridCols = computed(() => {
   return `grid-template-columns: ${props.checks ? '80px' : ''} repeat(${
     props.columns.length
   }, minmax(${props.minWidthColumn}, 1fr))`
 })
+
+watch(searchText, (value) => {
+  router.get(
+    'payment_methods',
+    { search: value },
+    {
+      preserveState: true,
+      replace: true
+    }
+  )
+})
 </script>
 
 <template>
-  <div v-if="Object.keys(data).length > 0" class="max-w-full overflow-x-auto">
+  <div v-if="search" class="mb-8 md:max-w-70">
+    <Input id="search" v-model="searchText" :placeholder="placeholderSearch" :is-search="true" />
+  </div>
+  <div class="max-w-full overflow-x-auto">
     <table class="w-full table-auto">
       <thead>
         <tr class="bg-gray-2 text-left dark:bg-meta-4 grid" :style="gridCols">
@@ -55,16 +85,16 @@ const gridCols = computed(() => {
           >
             <div class="flex gap-8">
               {{ head.name }}
-              <span v-if="head.short" class="cursor-pointer">
+              <span v-if="head.sort" class="cursor-pointer" @click="sort(head.keyValue)">
                 <SvgIcon name="Sort" />
               </span>
             </div>
           </th>
         </tr>
       </thead>
-      <tbody>
+      <tbody v-if="data.data.length !== 0">
         <tr
-          v-for="(item, i) in data"
+          v-for="(item, i) in data.data"
           :key="i"
           class="hover:bg-gray-2 dark:hover:bg-meta-4 duration-150 border-b border-[#eee] dark:border-[#eeeeee34] last:border-none grid"
           :style="gridCols"
@@ -121,10 +151,9 @@ const gridCols = computed(() => {
           </td>
         </tr>
       </tbody>
+      <div v-else>
+        <NoDataMessage :message="message" />
+      </div>
     </table>
-  </div>
-
-  <div v-else>
-    <NoDataMessage :message="message" />
   </div>
 </template>
