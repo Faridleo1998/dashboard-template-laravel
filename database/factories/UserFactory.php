b42d1aadeee1ce0bc9317ac5dff719ec75a9f9f1
Farid Moreno <farid.leonardo98@gmail.com>
2023-09-22 20:22:33 -0500

    feat(factories): add fields user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,11 +10,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'identification_number' => fake()->randomNumber(6, true),
+            'full_name' => fake()->name(),
+            'phone_number' => fake()->phoneNumber(),
+            'address' => fake()->address(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'status' => fake()->randomElement([0, 1]),
         ];
     }
 
