2c5d27d2a58b9375a9598e90e59d4d1137b561de
Farid Moreno <farid.leonardo98@gmail.com>
2023-09-28 17:58:05 -0500

    feat: set lowercase and get titlecase title

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,4 +15,12 @@ class PaymentMethod extends Model
         'created_at',
         'updated_at',
     ];
+
+    protected function title(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => ucwords($value),
+            set: fn ($value) => strtolower($value)
+        );
+    }
 }
