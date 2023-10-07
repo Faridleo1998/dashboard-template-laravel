7594c56b1a116900764eb8ad710edf0ec9178d5e
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-07 15:27:44 -0500

    feat(Services): add permission service

---
+++ app/Services/PermissionService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Helpers\FiltersTrait;
+use Illuminate\Database\Eloquent\Collection;
+use Spatie\Permission\Models\Permission;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class PermissionService
+{
+    use FiltersTrait;
+
+    protected $globalSearch;
+
+    public function __construct()
+    {
+        $this->globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                Collection::wrap($value)->each(function ($value) use ($query) {
+                    $query
+                        ->orWhere('description', 'LIKE', "%{$value}%")
+                        ->orWhere('module', 'LIKE', "%{$value}%");
+                });
+            });
+        });
+    }
+
+    public function getAll()
+    {
+        return QueryBuilder::for(Permission::class)
+            ->select('id', 'description', 'module')
+            ->defaultSort('module')
+            ->allowedSorts(['description', 'module'])
+            ->allowedFilters(['module'])
+            ->paginate(request()->get('perPage', 15))
+            ->withQueryString();
+    }
+}
