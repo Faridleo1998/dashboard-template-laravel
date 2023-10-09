719061570021cab454b495598211b8dbb79fe928
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-08 23:16:39 -0500

    feat(Components): add subtitle page component

---
+++ resources/js/Components/UI/SubtitlePage.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h4 class="mb-6 text-xl font-semibold text-black dark:text-white">{{ text }}</h4>
+</template>
