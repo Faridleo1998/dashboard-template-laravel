48a01dc4411b4435fee7faaece48e3e1bce8e6c1
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-08 23:14:21 -0500

    feat(routes): add roles route - admin

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\PaymentMethodController;
 use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,4 +12,5 @@ Route::get('/', function () {
 
 Route::resource('payment_methods', PaymentMethodController::class)->names('payment_methods');
 Route::resource('permissions', PermissionController::class)->names('permissions');
+Route::resource('roles', RoleController::class)->names('roles');
 Route::resource('users', UserController::class)->names('users');
