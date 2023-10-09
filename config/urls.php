87360ef1af16782db08370925d5bd7986287efef
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-09 17:53:40 -0500

    feat(config): add permissions field to routes - urls.php

--- config/urls.php
+++ config/urls.php
@@ -6,26 +6,31 @@ return [
             'name' => 'Medios de Pago',
             'route' => 'payment_methods.index',
             'icon' => 'wallet',
+            'permissions' => 'view payment_methods',
         ],
         [
             'name' => 'Configuración',
             'icon' => 'settings',
             'isGroup' => true,
+            'permissions' => ['view users', 'view roles', 'view permissions'],
             'children' => [
                 [
                     'name' => 'Usuarios',
                     'route' => 'users.index',
                     'icon' => 'users',
+                    'permissions' => 'view users',
                 ],
                 [
                     'name' => 'Roles',
                     'route' => 'roles.index',
                     'icon' => 'fingerprint',
+                    'permissions' => 'view roles',
                 ],
                 [
                     'name' => 'Permisos',
                     'route' => 'permissions.index',
                     'icon' => 'circle-key',
+                    'permissions' => 'view permissions',
                 ],
             ],
         ],
