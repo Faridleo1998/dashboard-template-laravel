777cf5dfc034f6710716748390fb7d0314d81980
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-08 23:02:05 -0500

    style(Controllers): remove empty functions - Permission

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Services\PermissionService;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -23,35 +22,5 @@ class PermissionController extends Controller
         $permissions = $this->permissions->getAll();
 
         return Inertia::render('Admin/Permissions/Index', compact('permissions', 'filters'));
-    }
-
-    public function create()
-    {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function show(string $id)
-    {
-        //
-    }
-
-    public function edit(string $id)
-    {
-        //
-    }
-
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy(string $id)
-    {
-        //
     }
 }
