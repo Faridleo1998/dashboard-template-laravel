c6da3bc03d83b176315f0b9ab2225700ff92eb86
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-08 23:13:01 -0500

    feat(Pages): add index, create and edit views - Roles

---
+++ resources/js/Pages/Admin/Roles/Create.vue
@@ -0,0 +1,162 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import Button from '@/Components/Buttons/Button.vue'
+import Card from '@/Components/Common/Card.vue'
+import Checkbox from '@/Components/Forms/Checkbox.vue'
+import Form from '@/Components/Forms/Form.vue'
+import Input from '@/Components/Forms/Input.vue'
+import SubtitlePage from '@/Components/UI/SubtitlePage.vue'
+import Switcher from '@/Components/Forms/Switcher.vue'
+import TitlePage from '@/Components/UI/TitlePage.vue'
+import { Head, useForm } from '@inertiajs/vue3'
+import { computed, ref } from 'vue'
+
+const props = defineProps({
+  permissions: {
+    type: Object,
+    default: () => {
+      return {}
+    }
+  }
+})
+
+defineOptions({
+  layout: AuthenticatedLayout
+})
+
+const form = useForm({
+  name: '',
+  status: 1,
+  permissions: []
+})
+
+const submit = () => {
+  form.post(route('roles.store'))
+}
+
+const isCheckAll = ref(false)
+
+const groupedPermissions = Object.values(
+  props.permissions.data.reduce((acc, obj) => {
+    const key = obj.module
+    if (!acc[key]) {
+      acc[key] = { module: key, data: [] }
+    }
+    acc[key].data.push(obj)
+    return acc
+  }, {})
+)
+
+const columnsPermissions = ['Módulo', 'Ver', 'Crear', 'Editar', 'Eliminar']
+
+const gridCols = computed(() => {
+  return `grid-template-columns: 300px repeat(${columnsPermissions.length - 1}, minmax(140px, 1fr))`
+})
+
+const handlePermissionsSelected = (id) => {
+  isCheckAll.value = false
+  if (isCheckedPermission(id)) {
+    form.permissions = form.permissions.filter((permission) => permission !== id)
+  } else {
+    form.permissions.push(id)
+  }
+}
+
+const isCheckedPermission = (id) => {
+  return form.permissions.some((permission) => {
+    return permission === id
+  })
+}
+
+const checkAllPermissions = () => {
+  isCheckAll.value = !isCheckAll.value
+  form.permissions = []
+
+  if (isCheckAll.value) {
+    props.permissions.data.map((permission) => form.permissions.push(permission.id))
+  }
+}
+</script>
+
+<template>
+  <Head title="Crear rol" />
+  <TitlePage title="Crear rol" />
+
+  <Card>
+    <Form>
+      <Input
+        id="name"
+        v-model="form.name"
+        label="Nombre"
+        :is-required="true"
+        :has-error="form.errors.name"
+        autofocus
+      />
+
+      <Switcher id="status" v-model="form.status" />
+    </Form>
+
+    <div class="mt-10">
+      <SubtitlePage text="Permisos" class="mb-4" />
+
+      <Checkbox
+        id="checkAll"
+        label="Seleccionar todo"
+        class="mb-4"
+        :checked="isCheckAll"
+        @click="checkAllPermissions"
+      />
+
+      <div class="max-w-full overflow-x-auto">
+        <table class="w-full table-auto">
+          <thead>
+            <tr class="bg-gray-2 text-left dark:bg-meta-4 grid" :style="gridCols">
+              <th
+                v-for="(head, i) in columnsPermissions"
+                :key="i"
+                class="py-5 px-4 font-medium text-black dark:text-white break-words grid place-content-center"
+              >
+                <div class="items-center flex h-full">
+                  {{ head }}
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody v-if="groupedPermissions.length !== 0" class="grid auto-rows-[60px]">
+            <tr
+              v-for="(item, i) in groupedPermissions"
+              :key="i"
+              class="duration-150 border-b border-[#eee] dark:border-[#eeeeee34] last:border-none grid items-center"
+              :style="gridCols"
+            >
+              <td class="grid pl-4">{{ item.module }}</td>
+              <td
+                v-for="permission in item.data"
+                :key="permission.id"
+                class="grid place-content-center"
+              >
+                <Checkbox
+                  :id="permission.id"
+                  :key="permission.id"
+                  :checked="isCheckedPermission(permission.id)"
+                  :value="permission.id"
+                  @click="handlePermissionsSelected(permission.id)"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <template #footer>
+      <div></div>
+      <div class="flex gap-6">
+        <Button :href="route('roles.index')" type="danger" icon="circle-half-vertical"
+          >Cancelar</Button
+        >
+        <Button icon="device-floppy" @click="submit">Crear</Button>
+      </div>
+    </template>
+  </Card>
+</template>
---
+++ resources/js/Pages/Admin/Roles/Index.vue
@@ -0,0 +1,90 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import Button from '@/Components/Buttons/Button.vue'
+import Card from '@/Components/Common/Card.vue'
+import Table from '@/Components/Common/Table.vue'
+import TitlePage from '@/Components/UI/TitlePage.vue'
+import { Head } from '@inertiajs/vue3'
+
+defineOptions({
+  layout: AuthenticatedLayout
+})
+
+const props = defineProps({
+  roles: {
+    type: Object,
+    required: true
+  },
+  filters: {
+    type: Object,
+    default: () => {}
+  }
+})
+
+const columns = [
+  {
+    name: 'Nombre',
+    keyValue: 'name',
+    type: 'default',
+    sort: true
+  },
+  {
+    name: 'Estado',
+    keyValue: 'status',
+    type: 'status',
+    sort: true
+  },
+  {
+    name: 'Acciones',
+    type: 'actions'
+  }
+]
+
+const filtersParsed = {
+  direction: props.filters.direction || '',
+  sort: props.filters.sort || '',
+  'filter[global]': props.filters.filter.global || '',
+  page: props.filters.page || 1,
+  perPage: parseInt(props.filters.perPage) || 15,
+  'filter[status]': props.filters?.filter.module || ''
+}
+
+const filterOptions = [
+  {
+    label: 'Estado',
+    keyValue: 'status',
+    type: 'select',
+    options: {
+      0: 'Inactivo',
+      1: 'Activo'
+    }
+  }
+]
+
+const currentRoute = route().current().split('.')[0]
+
+const routeActions = {
+  edit: 'roles.edit',
+  delete: 'roles.destroy'
+}
+</script>
+
+<template>
+  <Head title="Roles" />
+  <TitlePage title="Roles">
+    <Button icon="plus" :href="route('roles.create')"> Crear </Button>
+  </TitlePage>
+
+  <Card>
+    <Table
+      :columns="columns"
+      :current-route="currentRoute"
+      :data="roles"
+      :filter-options="filterOptions"
+      :filters="filtersParsed"
+      :route-actions="routeActions"
+      message="No se encontraron roles"
+      placeholder-search="Nombre"
+    />
+  </Card>
+</template>
