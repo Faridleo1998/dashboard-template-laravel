7a789509c62510df91e1a434fbaea02fc4e8ef5b
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-12 13:50:48 -0500

    feat(Layouts): add class to main tag - authenticated

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -19,8 +19,8 @@ const setSidebarOpen = (isOpen) => {
       <Sidebar :sidebar-open="sidebarOpen" @set-sidebar-open="setSidebarOpen" />
       <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
         <Header :sidebar-open="sidebarOpen" @set-sidebar-open="setSidebarOpen" />
-        <main>
-          <div class="mx-auto max-w-screen-3xl p-4 md:p-6 2xl:p-10">
+        <main class="h-full">
+          <div class="mx-auto max-w-screen-3xl p-4 md:p-6 2xl:p-10 h-full">
             <slot />
           </div>
         </main>
