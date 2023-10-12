c33637a78fd4048493755f80dc0d45f820668674
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-12 13:46:00 -0500

    feat(Policies): add payment method and user policies

---
+++ app/Policies/PaymentMethodPolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\Helpers\PolicyMessageTrait;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PaymentMethodPolicy
+{
+    use PolicyMessageTrait;
+
+    const NAMEITEM = 'medios de pago';
+
+    public function viewAny(User $user): Response
+    {
+        return $user->hasPermissionTo('view payment_methods')
+            ? Response::allow()
+            : Response::deny($this->getMessage(action: 'view', nameItem: self::NAMEITEM));
+    }
+
+    public function create(User $user): Response
+    {
+        return $user->hasPermissionTo('create payment_methods')
+            ? Response::allow()
+            : Response::deny($this->getMessage(action: 'create', nameItem: self::NAMEITEM));
+    }
+
+    public function update(User $user): Response
+    {
+        return $user->hasPermissionTo('edit payment_methods')
+            ? Response::allow()
+            : Response::deny($this->getMessage(action: 'edit', nameItem: self::NAMEITEM));
+    }
+
+    public function delete(User $user): Response
+    {
+        return $user->hasPermissionTo('delete payment_methods')
+            ? Response::allow()
+            : Response::deny($this->getMessage(action: 'delete', nameItem: self::NAMEITEM));
+    }
+}
