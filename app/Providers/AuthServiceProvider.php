cd2bd09347a1d7cc77235d29712be95c5132f378
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-12 14:05:13 -0500

    feat(Providers): import role policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,12 +5,15 @@ namespace App\Providers;
 use App\Models\PaymentMethod;
 use App\Models\User;
 use App\Policies\PaymentMethodPolicy;
+use App\Policies\RolePolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Spatie\Permission\Models\Role;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
+        Role::class => RolePolicy::class,
         User::class => UserPolicy::class,
         PaymentMethod::class => PaymentMethodPolicy::class,
     ];
