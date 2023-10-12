9f4ac2c2f0a4942d6c6d7db5ce8401fbd2e5141b
Farid Moreno <farid.leonardo98@gmail.com>
2023-10-12 13:49:23 -0500

    feat(Providers): import payment method and user policies - auth service

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,23 +2,19 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\PaymentMethod;
+use App\Models\User;
+use App\Policies\PaymentMethodPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
-        //
+        User::class => UserPolicy::class,
+        PaymentMethod::class => PaymentMethodPolicy::class,
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
         //
